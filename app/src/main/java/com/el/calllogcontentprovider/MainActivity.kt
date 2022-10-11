b8090d16598a37fa51ff88f026310b12000a91a7
IvoKara <ivo.i.karaneshev@gmail.com>
2022-10-11 14:49:38 +0300

    Check for read call permittion granted when open app

--- app/src/main/java/com/el/calllogcontentprovider/MainActivity.kt
+++ app/src/main/java/com/el/calllogcontentprovider/MainActivity.kt
@@ -14,5 +14,42 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(stillNotGrantedPermission()) {
+            ActivityCompat.requestPermissions(
+                this,
+                Array(1) {android.Manifest.permission.READ_CALL_LOG},
+                101
+            )
+        }
+        else {
+            displayLog()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(
+            requestCode == 101 &&
+            grantResults[0] == PackageManager.PERMISSION_GRANTED
+        ) {
+            displayLog()
+        }
+    }
+
+    private fun displayLog() {
+        
+    }
+
+    private fun stillNotGrantedPermission(): Boolean {
+        return ActivityCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.READ_CALL_LOG
+        ) != PackageManager.PERMISSION_GRANTED
     }
 }
