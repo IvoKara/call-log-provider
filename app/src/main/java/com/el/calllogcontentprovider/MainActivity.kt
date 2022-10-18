1e67d0f5b41d419598ccd59b1a388281dfe16058
IvoKara <ivo.i.karaneshev@gmail.com>
2022-10-18 08:09:49 +0300

    Showing logs on different cards

--- app/src/main/java/com/el/calllogcontentprovider/MainActivity.kt
+++ app/src/main/java/com/el/calllogcontentprovider/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.CallLog
+import android.widget.ListView
+import android.widget.SimpleCursorAdapter
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import java.util.jar.Manifest
@@ -14,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         if(stillNotGrantedPermission()) {
             ActivityCompat.requestPermissions(
@@ -43,7 +46,46 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun displayLog() {
-        
+        val colsFromContentProvider = listOf<String>(
+            CallLog.Calls._ID,
+            CallLog.Calls.NUMBER,
+            CallLog.Calls.TYPE,
+            CallLog.Calls.DURATION,
+            CallLog.Calls.LAST_MODIFIED,
+            CallLog.Calls.DATE,
+            CallLog.Calls.CACHED_NAME,
+            CallLog.Calls.CACHED_NUMBER_LABEL,
+        ).toTypedArray()
+
+        val from = listOf<String>(
+            CallLog.Calls.NUMBER,
+            CallLog.Calls.DURATION,
+            CallLog.Calls.TYPE
+        ).toTypedArray()
+
+        val to = intArrayOf(
+            R.id.textView1,
+            R.id.textView2,
+            R.id.textView3
+        )
+
+        val recentCalls = contentResolver.query(
+            CallLog.Calls.CONTENT_URI,
+            colsFromContentProvider,
+            null, null,
+            "${CallLog.Calls.LAST_MODIFIED} DESC"
+        )
+
+        val adapter = SimpleCursorAdapter(
+            applicationContext,
+            R.layout.activity_call_log,
+            recentCalls,
+            from, to, 0
+        )
+
+        val listview = findViewById<ListView>(R.id.listview)
+
+        listview.adapter = adapter
     }
 
     private fun stillNotGrantedPermission(): Boolean {
